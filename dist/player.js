46a295724f3fa77aee1aa86f3876c02784458f10
Kiwi <kiwi.sedna@gmail.com>
2014-10-30 22:44:53 +0800

    waiter timeout使用Timer.js去监听处理 原先简单setTimeout的实现在chrome浏览器Tab处于非主焦点Tab时，会有clearTimeout无法清理timer的bug

--- dist/player.js
+++ dist/player.js
@@ -208,6 +208,156 @@
   return utils;
 });
 
+// Timer.js: A periodic timer for Node.js and the browser.
+//
+// Copyright (c) 2012 Arthur Klepchukov, Jarvis Badgley, Florian Schäfer
+// Licensed under the BSD license (BSD_LICENSE.txt)
+//
+// Version: 0.0.1
+(function (root, factory) {
+    if (typeof exports === 'object') {
+        module.exports = factory();
+    } else if (typeof define === 'function' && define.amd) {
+        define('muplayer/lib/Timer',factory);
+    } else {
+        root._mu.Timer = factory();
+    }
+})(this, function () {
+    function timeStringToMilliseconds(timeString) {
+        if (typeof timeString === 'string') {
+
+            if (isNaN(parseInt(timeString, 10))) {
+                timeString = '1' + timeString;
+            }
+
+            var match = timeString
+                .replace(/[^a-z0-9\.]/g, '')
+                .match(/(?:(\d+(?:\.\d+)?)(?:days?|d))?(?:(\d+(?:\.\d+)?)(?:hours?|hrs?|h))?(?:(\d+(?:\.\d+)?)(?:minutes?|mins?|m\b))?(?:(\d+(?:\.\d+)?)(?:seconds?|secs?|s))?(?:(\d+(?:\.\d+)?)(?:milliseconds?|ms))?/);
+
+            if (match[0]) {
+                return parseFloat(match[1] || 0) * 86400000 +  // days
+                       parseFloat(match[2] || 0) * 3600000 +   // hours
+                       parseFloat(match[3] || 0) * 60000 +     // minutes
+                       parseFloat(match[4] || 0) * 1000 +      // seconds
+                       parseInt(match[5] || 0, 10);            // milliseconds
+            }
+
+            if (!isNaN(parseInt(timeString, 10))) {
+                return parseInt(timeString, 10);
+            }
+        }
+
+        if (typeof timeString === 'number') {
+            return timeString;
+        }
+
+        return 0;
+    }
+
+    function millisecondsToTicks(milliseconds, resolution) {
+        return parseInt(milliseconds / resolution, 10) || 1;
+    }
+
+    function Timer(resolution) {
+        if (this instanceof Timer === false) {
+            return new Timer(resolution);
+        }
+
+        this._notifications = [];
+        this._resolution = timeStringToMilliseconds(resolution) || 1000;
+        this._running = false;
+        this._ticks = 0;
+        this._timer = null;
+        this._drift = 0;
+    }
+
+    Timer.prototype = {
+        start: function () {
+            var self = this;
+            if (!this._running) {
+                this._running = !this._running;
+                setTimeout(function loopsyloop() {
+                    self._ticks++;
+                    for (var i = 0, l = self._notifications.length; i < l; i++) {
+                        if (self._notifications[i] && self._ticks % self._notifications[i].ticks === 0) {
+                            self._notifications[i].callback.call(self._notifications[i], { ticks: self._ticks, resolution: self._resolution });
+                        }
+                    }
+                    if (self._running) {
+                        self._timer = setTimeout(loopsyloop, self._resolution + self._drift);
+                        self._drift = 0;
+                    }
+                }, this._resolution);
+            }
+            return this;
+        },
+        stop: function () {
+            if (this._running) {
+                this._running = !this._running;
+                clearTimeout(this._timer);
+            }
+            return this;
+        },
+        reset: function () {
+            this.stop();
+            this._ticks = 0;
+            return this;
+        },
+        clear: function () {
+            this.reset();
+            this._notifications = [];
+            return this;
+        },
+        ticks: function () {
+            return this._ticks;
+        },
+        resolution: function () {
+            return this._resolution;
+        },
+        running: function () {
+            return this._running;
+        },
+        bind: function (when, callback) {
+            if (when && callback) {
+                var ticks = millisecondsToTicks(timeStringToMilliseconds(when), this._resolution);
+                this._notifications.push({
+                    ticks: ticks,
+                    callback: callback
+                });
+            }
+            return this;
+        },
+        unbind: function (callback) {
+            if (!callback) {
+                this._notifications = [];
+            } else {
+                for (var i = 0, l = this._notifications.length; i < l; i++) {
+                    if (this._notifications[i] && this._notifications[i].callback === callback) {
+                        this._notifications.splice(i, 1);
+                    }
+                }
+            }
+            return this;
+        },
+        drift: function (timeDrift) {
+            this._drift = timeDrift;
+            return this;
+        }
+    };
+
+    Timer.prototype.every = Timer.prototype.bind;
+    Timer.prototype.after = function (when, callback) {
+        var self = this;
+        Timer.prototype.bind.call(self, when, function fn () {
+            Timer.prototype.unbind.call(self, fn);
+            callback.apply(this, arguments);
+        });
+        return this;
+    };
+
+    return Timer;
+});
+
 (function (root, factory) {
   if (typeof exports === 'object') {
     module.exports = factory();
@@ -1172,156 +1322,6 @@ var __hasProp = {}.hasOwnProperty,
   return AudioCore;
 });
 
-// Timer.js: A periodic timer for Node.js and the browser.
-//
-// Copyright (c) 2012 Arthur Klepchukov, Jarvis Badgley, Florian Schäfer
-// Licensed under the BSD license (BSD_LICENSE.txt)
-//
-// Version: 0.0.1
-(function (root, factory) {
-    if (typeof exports === 'object') {
-        module.exports = factory();
-    } else if (typeof define === 'function' && define.amd) {
-        define('muplayer/lib/Timer',factory);
-    } else {
-        root._mu.Timer = factory();
-    }
-})(this, function () {
-    function timeStringToMilliseconds(timeString) {
-        if (typeof timeString === 'string') {
-
-            if (isNaN(parseInt(timeString, 10))) {
-                timeString = '1' + timeString;
-            }
-
-            var match = timeString
-                .replace(/[^a-z0-9\.]/g, '')
-                .match(/(?:(\d+(?:\.\d+)?)(?:days?|d))?(?:(\d+(?:\.\d+)?)(?:hours?|hrs?|h))?(?:(\d+(?:\.\d+)?)(?:minutes?|mins?|m\b))?(?:(\d+(?:\.\d+)?)(?:seconds?|secs?|s))?(?:(\d+(?:\.\d+)?)(?:milliseconds?|ms))?/);
-
-            if (match[0]) {
-                return parseFloat(match[1] || 0) * 86400000 +  // days
-                       parseFloat(match[2] || 0) * 3600000 +   // hours
-                       parseFloat(match[3] || 0) * 60000 +     // minutes
-                       parseFloat(match[4] || 0) * 1000 +      // seconds
-                       parseInt(match[5] || 0, 10);            // milliseconds
-            }
-
-            if (!isNaN(parseInt(timeString, 10))) {
-                return parseInt(timeString, 10);
-            }
-        }
-
-        if (typeof timeString === 'number') {
-            return timeString;
-        }
-
-        return 0;
-    }
-
-    function millisecondsToTicks(milliseconds, resolution) {
-        return parseInt(milliseconds / resolution, 10) || 1;
-    }
-
-    function Timer(resolution) {
-        if (this instanceof Timer === false) {
-            return new Timer(resolution);
-        }
-
-        this._notifications = [];
-        this._resolution = timeStringToMilliseconds(resolution) || 1000;
-        this._running = false;
-        this._ticks = 0;
-        this._timer = null;
-        this._drift = 0;
-    }
-
-    Timer.prototype = {
-        start: function () {
-            var self = this;
-            if (!this._running) {
-                this._running = !this._running;
-                setTimeout(function loopsyloop() {
-                    self._ticks++;
-                    for (var i = 0, l = self._notifications.length; i < l; i++) {
-                        if (self._notifications[i] && self._ticks % self._notifications[i].ticks === 0) {
-                            self._notifications[i].callback.call(self._notifications[i], { ticks: self._ticks, resolution: self._resolution });
-                        }
-                    }
-                    if (self._running) {
-                        self._timer = setTimeout(loopsyloop, self._resolution + self._drift);
-                        self._drift = 0;
-                    }
-                }, this._resolution);
-            }
-            return this;
-        },
-        stop: function () {
-            if (this._running) {
-                this._running = !this._running;
-                clearTimeout(this._timer);
-            }
-            return this;
-        },
-        reset: function () {
-            this.stop();
-            this._ticks = 0;
-            return this;
-        },
-        clear: function () {
-            this.reset();
-            this._notifications = [];
-            return this;
-        },
-        ticks: function () {
-            return this._ticks;
-        },
-        resolution: function () {
-            return this._resolution;
-        },
-        running: function () {
-            return this._running;
-        },
-        bind: function (when, callback) {
-            if (when && callback) {
-                var ticks = millisecondsToTicks(timeStringToMilliseconds(when), this._resolution);
-                this._notifications.push({
-                    ticks: ticks,
-                    callback: callback
-                });
-            }
-            return this;
-        },
-        unbind: function (callback) {
-            if (!callback) {
-                this._notifications = [];
-            } else {
-                for (var i = 0, l = this._notifications.length; i < l; i++) {
-                    if (this._notifications[i] && this._notifications[i].callback === callback) {
-                        this._notifications.splice(i, 1);
-                    }
-                }
-            }
-            return this;
-        },
-        drift: function (timeDrift) {
-            this._drift = timeDrift;
-            return this;
-        }
-    };
-
-    Timer.prototype.every = Timer.prototype.bind;
-    Timer.prototype.after = function (when, callback) {
-        var self = this;
-        Timer.prototype.bind.call(self, when, function fn () {
-            Timer.prototype.unbind.call(self, fn);
-            callback.apply(this, arguments);
-        });
-        return this;
-    };
-
-    return Timer;
-});
-
 (function (root, factory) {
     if (typeof exports === 'object') {
         module.exports = factory();
@@ -2199,11 +2199,11 @@ var __hasProp = {}.hasOwnProperty,
   if (typeof exports === 'object') {
     return module.exports = factory();
   } else if (typeof define === 'function' && define.amd) {
-    return define('muplayer/player',['muplayer/core/cfg', 'muplayer/core/utils', 'muplayer/lib/events', 'muplayer/core/playlist', 'muplayer/core/engines/engine'], factory);
+    return define('muplayer/player',['muplayer/core/cfg', 'muplayer/core/utils', 'muplayer/lib/Timer', 'muplayer/lib/events', 'muplayer/core/playlist', 'muplayer/core/engines/engine'], factory);
   } else {
-    return root._mu.Player = factory(root._mu.cfg, root._mu.utils, root._mu.Events, root._mu.Playlist, root._mu.Engine);
+    return root._mu.Player = factory(_mu.cfg, _mu.utils, _mu.Timer, _mu.Events, _mu.Playlist, _mu.Engine);
   }
-})(this, function(cfg, utils, Events, Playlist, Engine) {
+})(this, function(cfg, utils, Timer, Events, Playlist, Engine) {
   var EVENTS, Player, STATES, ctrl, time2str, _ref;
   _ref = cfg.engine, EVENTS = _ref.EVENTS, STATES = _ref.STATES;
   time2str = utils.time2str;
@@ -2255,7 +2255,7 @@ var __hasProp = {}.hasOwnProperty,
       singleton: true,
       absoluteUrl: true,
       maxRetryTimes: 1,
-      maxWaitingTime: 3 * 1000,
+      maxWaitingTime: 4 * 1000,
       recoverMethodWhenWaitingTimeout: 'retry'
     };
 
@@ -2316,6 +2316,7 @@ var __hasProp = {}.hasOwnProperty,
     function Player(options) {
       var baseDir, opts;
       this.opts = opts = $.extend({}, Player.defaults, options);
+      this.waitingTimer = new Timer(100);
       baseDir = opts.baseDir;
       if (baseDir === false) {
         baseDir = '';
@@ -2344,15 +2345,6 @@ var __hasProp = {}.hasOwnProperty,
       this.reset();
     }
 
-    Player.prototype._clearTimeout = function(name) {
-      var tid;
-      tid = ~~this[name];
-      if (tid) {
-        clearTimeout(tid);
-        return delete this[name];
-      }
-    };
-
     Player.prototype._initEngine = function(engine) {
       var opts, pass, recover, self;
       self = this;
@@ -2366,7 +2358,7 @@ var __hasProp = {}.hasOwnProperty,
         var st;
         st = e.newState;
         if (pass()) {
-          self._clearTimeout('waitingTimer');
+          self.waitingTimer.reset();
         }
         self.trigger('player:statechange', e);
         self.trigger(st);
@@ -2376,13 +2368,11 @@ var __hasProp = {}.hasOwnProperty,
       }).on(EVENTS.POSITIONCHANGE, function(pos) {
         self.trigger('timeupdate', pos);
         if (self.getUrl()) {
-          self._clearTimeout('waitingTimer');
-          return self.waitingTimer = setTimeout(function() {
+          return self.waitingTimer.clear().after(opts.maxWaitingTime, function() {
             if (!pass()) {
-              engine.trigger(EVENTS.WAITING_TIMEOUT);
+              return engine.trigger(EVENTS.WAITING_TIMEOUT);
             }
-            return self._clearTimeout('waitingTimer');
-          }, opts.maxWaitingTime);
+          }).start();
         }
       }).on(EVENTS.PROGRESS, function(progress) {
         return self.trigger('progress', progress);
