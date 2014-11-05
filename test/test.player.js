8cc6b8aa3a807775b7eb1b512a1ec775b6ed2726
Kiwi <kiwi.sedna@gmail.com>
2014-11-05 16:45:02 +0800

    补充对pause和stop的测试

--- test/test.player.js
+++ test/test.player.js
@@ -5,6 +5,8 @@ var p = new _mu.Player({
     }),
     mp3 = '/st/mp3/rain.mp3';
 
+window.muplayer = p;
+
 suite('player', function() {
     suite('#play()', function() {
         test('播放开始后会派发playing事件', function(done) {
@@ -17,14 +19,48 @@ suite('player', function() {
     });
 
     suite('#pause()', function() {
-        test('暂停后会派发pause事件', function(done) {
+        test('暂停播放后会派发pause事件', function(done) {
             p.on('playing', function() {
-                assert.ok(true);
                 p.pause();
-                done();
             });
             p.on('pause', function() {
                 assert.ok(true);
+                done();
+            });
+            p.setUrl(mp3).play();
+        });
+
+        test('暂停后播放位置不会被重置', function(done) {
+            p.on('timeupdate', function() {
+                p.pause();
+            });
+            p.on('pause', function() {
+                assert.ok(p.curPos() > 0);
+                done();
+            });
+            p.setUrl(mp3).play();
+        });
+    });
+
+    suite('#stop()', function() {
+        test('停止后会派发suspend事件', function(done) {
+            p.on('playing', function() {
+                p.stop();
+            });
+            p.on('suspend', function() {
+                assert.ok(true);
+                done();
+            });
+            p.setUrl(mp3).play();
+        });
+
+        test('停止播放会将当前播放位置重置', function(done) {
+            p.once('timeupdate', function() {
+                p.stop();
+            });
+            p.on('suspend', function() {
+                assert.equal(0, p.curPos());
+                done();
             });
             p.setUrl(mp3).play();
         });
