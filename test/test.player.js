b1b2ab0cdf5906412af8bdcb751da0fe05fb2703
Kiwi <kiwi.sedna@gmail.com>
2014-11-05 12:22:00 +0800

    修复reset后setCur失效的bug

--- test/test.player.js
+++ test/test.player.js
@@ -1,8 +1,11 @@
 var p = new _mu.Player({
         mute: true,
-        mode: 'list'
+        mode: 'list',
+        absoluteUrl: false
     }),
     mp3Path = '/st/mp3/';
+
+window.muplayer = p;
 
 suite('player', function() {
     setup(function() {
@@ -12,12 +15,32 @@ suite('player', function() {
     });
 
     suite('#play()', function() {
-        test('should trigger playing event when play', function(done) {
+        test('播放开始后会派发playing事件', function(done) {
             p.on('playing', function() {
                 assert.ok(true);
                 done();
             });
             p.play();
+        });
+    });
+
+    suite('#setCur()', function() {
+        test('通过setCur设置sid后，可通过getCur取回该sid', function() {
+            p.add([
+                '1', '2', '3'
+            ]).setCur('2');
+            assert.equal('2', p.getCur());
+        });
+
+        test('重置播放列表后可以通过setCur设置到指定sid', function() {
+            p.add([
+                '1', '2', '3', '4', '5'
+            ]).setCur('3');
+            assert.equal('3', p.getCur());
+            p.reset().add([
+                '1', '2', '3', '4', '5'
+            ]).setCur('3');
+            assert.equal('3', p.getCur());
         });
     });
 
