4698f22f0fb2e977614fae9e1100313566fa5202
Kiwi <kiwi.sedna@gmail.com>
2015-01-16 16:33:53 +0800

    hotfix

--- dist/zepto-player.js
+++ dist/zepto-player.js
@@ -1840,7 +1840,7 @@ var __slice = [].slice;
         } else {
           trigger(nst, e);
           if (ost === STATES.PAUSE || ost === STATES.PLAYING) {
-            self.setState(ost);
+            self.engine.setState(ost);
           }
         }
         if (nst === STATES.END) {
