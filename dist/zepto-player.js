d59a822ecd183097859a4d0bb5693920e2b6fd5b
Kiwi <kiwi.sedna@gmail.com>
2015-01-21 16:36:04 +0800

    fix #73

--- dist/zepto-player.js
+++ dist/zepto-player.js
@@ -3,7 +3,57 @@
 // -------------------------
 // (c) 2014 FE Team of Baidu Music
 // Can be freely distributed under the BSD license.
-(function(root, factory) {
+;(function($) {
+    var ObjProto = Object.prototype,
+        toString = ObjProto.toString;
+
+    $.isString = function(obj) {
+        return toString.call(obj) === '[object String]';
+    }
+
+    $.isNumeric = function(obj) {
+        return toString.call(obj) === '[object Number]';
+    }
+
+    // 参考: https://github.com/dexteryy/OzJS/blob/master/oz.js
+    $.getScript = function(url, op) {
+        var doc = document,
+            s = doc.createElement('script');
+            s.async = 'async';
+
+        if (!op) {
+            op = {};
+        } else if ($.isFunction(op)) {
+            op = {
+                callback: op
+            };
+        }
+
+        if (op.charset) {
+            s.charset = op.charset;
+        }
+
+        s.src = url;
+
+        var h = doc.getElementsByTagName('head')[0];
+
+        s.onload = s.onreadystatechange = function(__, isAbort) {
+            if (isAbort || !s.readyState || /loaded|complete/.test(s.readyState)) {
+                s.onload = s.onreadystatechange = null;
+                if (h && s.parentNode) {
+                    h.removeChild(s);
+                }
+                s = undefined;
+                if (!isAbort && op.callback) {
+                    op.callback();
+                }
+            }
+        };
+
+        h.insertBefore(s, h.firstChild);
+    }
+})(Zepto);
+;(function(root, factory) {
   if (typeof root._mu === 'undefined') {
     root._mu = {};
   }
@@ -1869,6 +1919,8 @@ var __slice = [].slice;
         this.opts.fetch.call(this).done(function() {
           play();
           return self.trigger('player:fetch:done');
+        }).fail(function(err) {
+          return self.trigger('player:fetch:fail', err);
         });
       } else {
         play();
@@ -2232,53 +2284,3 @@ var __slice = [].slice;
   return Player;
 });
 
-;;(function($) {
-    var ObjProto = Object.prototype,
-        toString = ObjProto.toString;
-
-    $.isString = function(obj) {
-        return toString.call(obj) === '[object String]';
-    }
-
-    $.isNumeric = function(obj) {
-        return toString.call(obj) === '[object Number]';
-    }
-
-    // 参考: https://github.com/dexteryy/OzJS/blob/master/oz.js
-    $.getScript = function(url, op) {
-        var doc = document,
-            s = doc.createElement('script');
-            s.async = 'async';
-
-        if (!op) {
-            op = {};
-        } else if ($.isFunction(op)) {
-            op = {
-                callback: op
-            };
-        }
-
-        if (op.charset) {
-            s.charset = op.charset;
-        }
-
-        s.src = url;
-
-        var h = doc.getElementsByTagName('head')[0];
-
-        s.onload = s.onreadystatechange = function(__, isAbort) {
-            if (isAbort || !s.readyState || /loaded|complete/.test(s.readyState)) {
-                s.onload = s.onreadystatechange = null;
-                if (h && s.parentNode) {
-                    h.removeChild(s);
-                }
-                s = undefined;
-                if (!isAbort && op.callback) {
-                    op.callback();
-                }
-            }
-        };
-
-        h.insertBefore(s, h.firstChild);
-    }
-})(Zepto);
